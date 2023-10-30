6357f483f42e4d3c8c36521e9fc1d10ce31cf868
EZP26 <ezpham1@gmail.com>
2023-10-30 14:01:17 -0700

    Moved out of public file

---
+++ sketch.js
@@ -0,0 +1,43 @@
+const rows = 100;
+const cols = 100;
+const cellSize = 4; // Adjust the cell size as needed
+const wallThickness = 2; // Adjust the wall thickness as needed
+
+const maze = [];
+
+function setup() {
+  createCanvas(cols * cellSize, rows * cellSize);
+  generateMaze();
+  drawMaze();
+}
+
+function generateMaze() {
+  // Your maze generation logic goes here
+  // Make sure to set wall information in the maze data structure
+}
+
+function drawMaze() {
+  background(255); // Set the background color
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const cell = maze[i][j];
+      const x = j * cellSize;
+      const y = i * cellSize;
+
+      // Draw walls based on the wall information in the maze data structure
+      if (cell.walls[0]) {
+        rect(x, y, cellSize, wallThickness); // Top wall
+      }
+      if (cell.walls[1]) {
+        rect(x + cellSize - wallThickness, y, wallThickness, cellSize); // Right wall
+      }
+      if (cell.walls[2]) {
+        rect(x, y + cellSize - wallThickness, cellSize, wallThickness); // Bottom wall
+      }
+      if (cell.walls[3]) {
+        rect(x, y, wallThickness, cellSize); // Left wall
+      }
+    }
+  }
+}
