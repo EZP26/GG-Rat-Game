c57b4375be3335b5ddbbe463edd6250892f7a0aa
EZP26 <ezpham1@gmail.com>
2023-10-30 13:24:33 -0700

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -2,12 +2,6 @@ function setup() {
   createCanvas(400, 400);
   let w1 = new Walls(50, 50, 5);
   w1.getLocation();
-
-  // Send log message to the server
-  fetch('/log?message=Log message from p5.js')
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
 }
 
 function draw() {
