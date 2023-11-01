807b782e120316b1c74cae765e72fb734d487583
EZP26 <ezpham1@gmail.com>
2023-11-01 11:09:09 -0700

    d

--- sketch.js
+++ sketch.js
@@ -1,43 +1,186 @@
-const rows = 100;
-const cols = 100;
-const cellSize = 4; // Adjust the cell size as needed
-const wallThickness = 2; // Adjust the wall thickness as needed
-
-const maze = [];
+let maze = null;
+let res = 10;
 
 function setup() {
-  createCanvas(cols * cellSize, rows * cellSize);
-  generateMaze();
+  createCanvas(800,800);
+
+  noStroke();
+
+  makeMaze(width/res + 2, height/res +2);
   drawMaze();
 }
-
-function generateMaze() {
-  // Your maze generation logic goes here
-  // Make sure to set wall information in the maze data structure
+let count = 0;
+function draw(){
+  if(count % 5 == 0){
+    if(maze.stack.length != 0){
+      background("#4A1D91");
+      mazeIterate();
+      drawMaze();
+    }
+  }
+  count++;
 }
 
-function drawMaze() {
-  background(255); // Set the background color
+function makeMaze(w, h){
+  maze = 
+  {
+    "stack": [],
+    "tiles": [],
+    "w":w,
+    "h":h,
+  }
 
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      const cell = maze[i][j];
-      const x = j * cellSize;
-      const y = i * cellSize;
-
-      // Draw walls based on the wall information in the maze data structure
-      if (cell.walls[0]) {
-        rect(x, y, cellSize, wallThickness); // Top wall
+  for(let i = 0; i < w; i++)
+  {
+    maze.tiles[i] = [];
+    for(let j = 0; j < h; j++)
+    {
+      maze.tiles[i][j] =
+      {
+        "up":"wall",
+        "down":"wall",
+        "left":"wall",
+        "right":"wall",
+        "isStart":false,
+        "isCurrent":false,
+        "x":i,
+        "y":j,
+        "seen":false,
       }
-      if (cell.walls[1]) {
-        rect(x + cellSize - wallThickness, y, wallThickness, cellSize); // Right wall
-      }
-      if (cell.walls[2]) {
-        rect(x, y + cellSize - wallThickness, cellSize, wallThickness); // Bottom wall
-      }
-      if (cell.walls[3]) {
-        rect(x, y, wallThickness, cellSize); // Left wall
+      if(i == 0 || i == w - 1 || j == 0 || j == h -1){
+        maze.tiles[i][j].seen = true;
       }
     }
+  }
+  maze.tiles[1][1].isCurrent = true;
+  maze.tiles[1][1].isStart = true;
+  maze.tiles[1][1].seen = true;
+  maze.stack.push(maze.tiles[1][1]);
+}
+
+function mazeIterate(){
+  let current = maze.stack.pop();
+  let tileAndWall = pickNeigbhor(current);
+  if (tileAndWall){
+    maze.stack.push(current);
+  tileAndWall.tile[tileAndWall.wall] = "open";
+  current[oppositeWall(tileAndWall.wall)] = "open";
+  tileAndWall = true;
+  maze.stack.push(tileAndWall.tile);
+  
+  current.isCurrent = false;
+  tileAndWall.tile.isCurrent = true;
+  }
+  else if(maze.stack.length != 0){
+    current.isCurrent = false;
+    maze.stack[maze.stack.length-1].isCurrent = true;
+  }
+}
+
+function pickNeigbhor(tile){
+  let unSeen = [];
+  
+  let upTile = maze.tiles[tilel.x][tile.y+1];
+  if(!upTile.seen)
+  {
+    unSeen.push({
+      "tile": upTile,
+      "wall": "up"
+    });
+  }
+
+  let downTile = maze.tiles[tilel.x][tile.y+1];
+  if(!downTile.seen)
+  {
+    unSeen.push({
+      "tile": downTile,
+      "wall": "down"
+    });
+  }
+
+  let rightTile = maze.tiles[tilel.x][tile.y+1];
+  if(!rightTile.seen)
+  {
+    unSeen.push({
+      "tile": rightTile,
+      "wall": "right"
+    });
+  }
+
+  let leftTile = maze.tiles[tilel.x][tile.y+1];
+  if(!leftTile.seen)
+  {
+    unSeen.push({
+      "tile": leftTile,
+      "wall": "left"
+    });
+  }
+
+  if(unSeen.length == 0){
+    return null;
+  }
+
+  return unSeen[Math.floor(Math.random()*unSeen.length)];
+}
+
+function oppositeWall(wall){
+  if(wall == "up")
+  {
+    return "down";
+  } 
+  else if(wall == "down")
+  {
+    return "up";
+  } 
+  else if(wall == "left")
+  {
+    return "right";
+  } 
+  else if(wall == "right")
+  {
+    return "left";
+  }
+
+  return -1;
+}
+
+function drawMaze(){
+  push();
+  translate(-res, -res);
+  for(let i = 0; i < maze.tiles.length; i++){
+    for(let j = 0; j < maze.tiels[i].length; j++){
+      let tile = maze.tiles[i][j];
+      drawTile(tile, i, j);
+    }
+  }
+  pop();
+}
+
+function drawTile(tile, i, j){
+  strokeWeight(0);
+
+  if(tile.seen == true){
+    fill(0);
+    square(i*res, j*res, res);
+    
+    strokeWeight(2);
+    stroke("white");
+    if(tile.up == "wall")
+    {
+      line((i)*res, (j)*res, (i+1)*res, (j)*res);
+    }
+    if(tile.down == "wall")
+    {
+      line((i)*res, (j+1)*res, (i+1)*res, (j+1)*res);
+    }
+    if(tile.right == "wall")
+    {
+      line((i)*res, (j)*res, (i)*res, (j+1)*res);
+    }
+    if(tile.left == "wall")
+    {
+      line((i+1)*res, (j)*res, (i+1)*res, (j+1)*res);
+    }
+
   }
 }
