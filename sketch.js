7eb0f02ef074eb2d88ed84e3f9b9f86091bc9738
EZP26 <ezpham1@gmail.com>
2023-11-14 11:17:33 -0800

    Save

--- sketch.js
+++ sketch.js
@@ -2,191 +2,151 @@ let maze = null;
 let res = 50;
 let count = 0;
 let tileCount = 0;
+let mouseXPos = 0;
+let mouseYPos = 0;
 
-function setup()
-{
+function setup() {
   createCanvas(1600, 900);
-  
   noStroke();
-  
-  makeMaze(width/res + 2, height/res + 2);
-  
+  makeMaze(width / res + 2, height / res + 2);
   drawMaze();
 }
 
-function draw()
-{
-  startGame();
-}
-
-function startGame(){
-  //if(count % 3 == 0){
-    while(maze.stack.length != 0)
-    {
-      background("#aee68e");
-      mazeIterate();
-      drawMaze();
-    }
-  //}
+function draw() {
+  while (maze.stack.length != 0) {
+    background("#aee68e");
+    mazeIterate();
+    drawMaze();
+  }
   count++;
 }
 
-function makeMaze(w, h)
-{
-  maze = 
-  {
-    "stack":[],
-    "tiles":[],
-    "w":w,
-    "h":h
+function makeMaze(w, h) {
+  maze = {
+    stack: [],
+    tiles: [],
+    w: w,
+    h: h,
   };
-  
-  for(let i = 0; i < w; i++)
-  {
+
+  for (let i = 0; i < w; i++) {
     maze.tiles[i] = [];
-    for(let j = 0; j < h; j++)
-    {
-      maze.tiles[i][j] = 
-      {
-        "up":"wall",
-        "down":"wall",
-        "left":"wall",
-        "right":"wall",
-        "isStart":false,
-        "isCurrent":false,
-        "x":i,
-        "y":j,
-        "seen":false
-      }
-      if(i == 0 || i == w-1 || 
-         j == 0 || j == h-1)
-      {
+    for (let j = 0; j < h; j++) {
+      maze.tiles[i][j] = {
+        up: "wall",
+        down: "wall",
+        left: "wall",
+        right: "wall",
+        isStart: false,
+        isCurrent: false,
+        x: i,
+        y: j,
+        seen: false,
+      };
+      if (i == 0 || i == w - 1 || j == 0 || j == h - 1) {
         maze.tiles[i][j].seen = true;
       }
     }
   }
-  
+
   maze.tiles[1][1].isCurrent = true;
   maze.tiles[1][1].isStart = true;
   maze.tiles[1][1].seen = true;
   maze.stack.push(maze.tiles[1][1]);
 }
 
-function mazeIterate()
-{
+function drawMouse() {
+  fill("gray");
+  noStroke();
+  circle(mouseXPos * res + res / 2, mouseYPos * res + res / 2, res / 2);
+}
+
+function mazeIterate() {
   let current = maze.stack.pop();
-  
+
   let tileAndWall = pickNeighbor(current);
-  if(tileAndWall)
-  {
+  if (tileAndWall) {
     maze.stack.push(current);
     tileAndWall.tile[tileAndWall.wall] = "open";
-    current[opositeWall(
-      tileAndWall.wall)] = "open";
+    current[opositeWall(tileAndWall.wall)] = "open";
     tileAndWall.tile.seen = true;
     maze.stack.push(tileAndWall.tile);
-    
+
+    maze.tiles[mouseXPos][mouseYPos].isCurrent = false;
+
     current.isCurrent = false;
     tileAndWall.tile.isCurrent = true;
 
     tileCount++;
 
-    if(tileCount % 30 == 0){
+    if (tileCount % 30 == 0) {
       drawCheese(tileAndWall.tile.x, tileAndWall.tile.y);
     }
-  }
-  else if(maze.stack.length != 0)
-  {
+  } else if (maze.stack.length != 0) {
     current.isCurrent = false;
-    maze.stack[
-      maze.stack.length-1].isCurrent = true;
+    maze.stack[maze.stack.length - 1].isCurrent = true;
   }
 }
 
-function drawCheese(i, j) {
-  fill("yellow");
-  noStroke();
-  circle(i*res + res/2, j*res + res/2, res/3);
-}
 
-function pickNeighbor(tile)
-{
+function pickNeighbor(tile) {
   let unSeen = [];
-  
-  let upTile = maze.tiles[tile.x][tile.y+1];
-  if(!upTile.seen)
-  {
+
+  let upTile = maze.tiles[tile.x][tile.y + 1];
+  if (!upTile.seen) {
     unSeen.push({
-      "tile":upTile,
-      "wall":"up"
+      tile: upTile,
+      wall: "up",
     });
   }
-  let downTile = maze.tiles[tile.x][tile.y-1];
-  if(!downTile.seen)
-  {
+  let downTile = maze.tiles[tile.x][tile.y - 1];
+  if (!downTile.seen) {
     unSeen.push({
-      "tile":downTile,
-      "wall":"down"
+      tile: downTile,
+      wall: "down",
     });
   }
-  let rightTile = maze.tiles[tile.x+1][tile.y];
-  if(!rightTile.seen)
-  {
+  let rightTile = maze.tiles[tile.x + 1][tile.y];
+  if (!rightTile.seen) {
     unSeen.push({
-      "tile":rightTile,
-      "wall":"right"
+      tile: rightTile,
+      wall: "right",
     });
   }
-  let leftTile = maze.tiles[tile.x-1][tile.y];
-  if(!leftTile.seen)
-  {
+  let leftTile = maze.tiles[tile.x - 1][tile.y];
+  if (!leftTile.seen) {
     unSeen.push({
-      "tile":leftTile,
-      "wall":"left"
+      tile: leftTile,
+      wall: "left",
     });
   }
-  
-  if(unSeen.length == 0)
-  {
+
+  if (unSeen.length == 0) {
     return null;
   }
-  
-  return unSeen[Math.floor(
-    Math.random()*unSeen.length
-  )];
+
+  return unSeen[Math.floor(Math.random() * unSeen.length)];
 }
 
-function opositeWall(wall)
-{
-  if(wall == "up")
-  {
-    return "down"
+function opositeWall(wall) {
+  if (wall == "up") {
+    return "down";
+  } else if (wall == "down") {
+    return "up";
+  } else if (wall == "right") {
+    return "left";
+  } else if (wall == "left") {
+    return "right";
   }
-  else if(wall == "down")
-  {
-    return "up"
-  }
-  else if(wall == "right")
-  {
-    return "left"
-  }
-  else if(wall == "left")
-  {
-    return "right"
-  }
-  
+
   return -1;
 }
 
-function drawMaze()
-{
+function drawMaze() {
   push();
   translate(-res, -res);
-  for(let i = 0; i < maze.tiles.length; i++)
-  {
-    for(let j = 0; 
-        j < maze.tiles[i].length; j++)
-    {
+  for (let i = 0; i < maze.tiles.length; i++) {
+    for (let j = 0; j < maze.tiles[i].length; j++) {
       let tile = maze.tiles[i][j];
       drawTile(tile, i, j);
     }
@@ -194,44 +154,42 @@ function drawMaze()
   pop();
 }
 
-function drawTile(tile, i, j)
-{
+function drawTile(tile, i, j) {
   strokeWeight(0);
-  
-  if(tile.seen == true)
-  {
+
+  if (tile.seen == true) {
     fill("#e0ecff");
-    square(i*res, j*res, res);
-    
+    square(i * res, j * res, res);
+
     strokeWeight(2);
     stroke("black");
-    if(tile.up == "wall")
-    {
-      line((i)*res, (j)*res, 
-           (i+1)*res, (j)*res);
+    if (tile.up == "wall") {
+      line(i * res, j * res, (i + 1) * res, j * res);
     }
-    if(tile.down == "wall")
-    {
-      line((i)*res, (j+1)*res, 
-           (i+1)*res, (j+1)*res);
+    if (tile.down == "wall") {
+      line(i * res, (j + 1) * res, (i + 1) * res, (j + 1) * res);
     }
-    if(tile.left == "wall")
-    {
-      line((i+1)*res, (j)*res, 
-           (i+1)*res, (j+1)*res);
+    if (tile.left == "wall") {
+      line((i + 1) * res, j * res, (i + 1) * res, (j + 1) * res);
     }
-    if(tile.right == "wall")
-    {
-      line((i)*res, (j)*res, 
-           (i)*res, (j+1)*res);
+    if (tile.right == "wall") {
+      line(i * res, j * res, i * res, (j + 1) * res);
     }
   }
+}
+
+function moveMouse(){
   
-  if(tile.isCurrent)
-  {
-    fill("orange")
-    noStroke();
-    circle(i*res + res/2, 
-             j*res + res/2, res/3);
+}
+
+function keyPresed(){
+  if (keyCode === UP_ARROW || "w"){
+    
+  } else if (keyCode === DOWN_ARROW || "s"){
+    
+  } else if (keyCode === RIGHT_ARROW || "d"){
+    
+  } else if (keyCode === LEFT_ARROW || "a"){
+    
   }
 }
